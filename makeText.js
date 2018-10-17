c80f76fb86f58494bb33ca411fe1b93f43ba2a59
Sarah Kaplan <skaplan520@gmail.com>
2018-10-16 17:26:21 -0700

    can combine multiple files or urls to mashup texts

--- makeText.js
+++ makeText.js
@@ -5,9 +5,16 @@ const fs = require('fs');
 const process = require('process');
 const axios = require('axios');
 
-async function makeTextFromFile(path, maxWords) {
+async function makeTextFromFile(paths, maxWords) {
   try {
-    let text = await fs.promises.readFile(path, 'utf-8');
+    let text = '';
+    for (let i = 0; i < paths.length; i++) {
+      text += await fs.promises.readFile(paths[i], 'utf-8');
+    }
+
+    //OLD WAY
+    // let text = await fs.promises.readFile(path, 'utf-8');
+
     let markov = new MarkovMachine(text);
     return console.log(markov.makeText(maxWords));
   } catch (err) {
@@ -16,11 +23,18 @@ async function makeTextFromFile(path, maxWords) {
   }
 }
 
-function makeTextFromUrl(url, maxWords) {
-  axios
-    .get(url)
+function makeTextFromUrl(urls, maxWords) {
+  let promises = [];
+  let text = '';
+  for (let i = 0; i < urls.length; i++) {
+    promises.push(axios.get(urls[i]));
+  }
+  Promise.all(promises)
     .then(function(resp) {
-      let markov = new MarkovMachine(resp.data);
+      for (let j = 0; j < resp.length; j++) {
+        text += resp[j].data;
+      }
+      let markov = new MarkovMachine(text);
       return console.log(markov.makeText(maxWords));
     })
     .catch(function(e) {
@@ -31,10 +45,16 @@ function makeTextFromUrl(url, maxWords) {
     });
 }
 
-let source = process.argv[3];
-
 if (process.argv[2] === 'file') {
-  makeTextFromFile(source, 200);
+  let args = [];
+  for (let i = 3; i < process.argv.length; i++) {
+    args.push(process.argv[i]);
+  }
+  makeTextFromFile(args, 200);
 } else {
-  makeTextFromUrl(source, 200);
+  let args = [];
+  for (let i = 3; i < process.argv.length; i++) {
+    args.push(process.argv[i]);
+  }
+  makeTextFromUrl(args, 200);
 }
